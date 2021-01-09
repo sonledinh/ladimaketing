5be52aeaa87c92c5baa96f977393ceeb1bb8f072
sonledinh <sonledinh@admicro.vn>
2021-01-09 20:43:46 +0700

    substrate

---
+++ js/private.js
@@ -0,0 +1,13 @@
+if($(window).innerWidth() > 767){}
+
+	$('.slide-text-loop').slick({
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrow: false,
+    dots: false,
+    slidesToShow: 1,
+    vertical: true,
+    verticalSwiping: true,
+    prevArrow: '',
+    nextArrow: '',
+}); 
